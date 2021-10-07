2029d00759aaf5ea55c55e3eaa0ca5692d641c94
immqu <immanuel.kunz@aisec.fraunhofer.de>
2021-10-07 17:00:39 +0200

    Pass updates (#27)
    Co-authored-by: Christian Banse <christian.banse@aisec.fraunhofer.de>

---
+++ cloudpg/src/main/java/io/clouditor/graph/passes/LogPass.kt
@@ -0,0 +1,43 @@
+package io.clouditor.graph.passes
+
+import de.fraunhofer.aisec.cpg.TranslationResult
+import de.fraunhofer.aisec.cpg.graph.declarations.TranslationUnitDeclaration
+import de.fraunhofer.aisec.cpg.graph.statements.expressions.MemberCallExpression
+import de.fraunhofer.aisec.cpg.passes.Pass
+import io.clouditor.graph.*
+
+abstract class LogPass : Pass() {
+    override fun cleanup() {}
+
+    protected fun handleLog(
+        t: TranslationResult,
+        m: MemberCallExpression,
+        name: String,
+        tu: TranslationUnitDeclaration
+    ) {
+        // get the application this is running in
+        val application = t.findApplicationByTU(tu)
+
+        // we assume that this is going to std out, for example resulting in log collection in
+        // k8s; the kubernetes resource is only connected if it found
+
+        // check, if application runs somewhere with resource logging
+        val log =
+            application?.runsOn?.firstOrNull()?.nextDFG?.filterIsInstance<ResourceLogging>()?.map {
+                it
+            }
+                ?: emptyList()
+
+
+        val out = LogOutput(m, log as List<Logging>, m.arguments.firstOrNull())
+        out.location = m.location
+        out.name = name
+
+        // add DFG from expression to sink
+        out.to.forEach { out.value.nextDFG.add((it)) }
+
+        t += out
+
+        application?.functionalities?.plusAssign(out)
+    }
+}
---
+++ cloudpg/src/main/java/io/clouditor/graph/passes/python/PythonLogPass.kt
@@ -0,0 +1,27 @@
+package io.clouditor.graph.passes.python
+
+import de.fraunhofer.aisec.cpg.TranslationResult
+import de.fraunhofer.aisec.cpg.graph.Node
+import de.fraunhofer.aisec.cpg.graph.statements.expressions.MemberCallExpression
+import de.fraunhofer.aisec.cpg.processing.IVisitor
+import de.fraunhofer.aisec.cpg.processing.strategy.Strategy
+import io.clouditor.graph.*
+import io.clouditor.graph.passes.LogPass
+
+class PythonLogPass : LogPass() {
+    override fun accept(t: TranslationResult) {
+        // if (this.lang is PythonLanguageFrontend) {
+        for (tu in t.translationUnits) {
+            tu.accept(
+                Strategy::AST_FORWARD,
+                object : IVisitor<Node?>() {
+                    fun visit(m: MemberCallExpression) {
+                        if (m.name == "info" && m.base.name == "logging") {
+                            handleLog(t, m, m.name, tu)
+                        }
+                    }
+                }
+            )
+        }
+    }
+}
