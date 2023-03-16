1cfe355913f7e6e8b5809e98a05f716bf3ddec61
andreas <andreas1.binder@gmail.com>
2023-03-16 16:44:20 +0100

    add: updated python validation for Art. 17

--- ppg-testing-library/GDPRComplianceChecks/RightToErasure/Python_validation/client.py
+++ ppg-testing-library/GDPRComplianceChecks/RightToErasure/Python_validation/client.py
@@ -2,14 +2,21 @@
 
 import requests
 
-def delete_own_data():
+def delete_own_data(personal_data):
     url = 'test-online-notepad.com/data'
+    requests.delete(url, json = personal_data)
+
+def send_data_to_external_advertising_server(personal_data):
+    url = 'test-online-notepad.com/data'
+    requests.post(url, json = personal_data)
+
+
+if __name__ == '__main__':
     #@PseudoIdentifier
     personal_data = {
         "username": "testuser",
         "notes": ["note1", "note2", "note3"]
     }
-    requests.delete(url, json = personal_data)
+    send_data_to_external_advertising_server(personal_data)
+    delete_own_data(personal_data)
 
-if __name__ == '__main__':
-    delete_own_data()
--- ppg-testing-library/GDPRComplianceChecks/RightToErasure/Python_validation/server.py
+++ ppg-testing-library/GDPRComplianceChecks/RightToErasure/Python_validation/server.py
@@ -23,5 +23,22 @@ def parse_data():
         # VALIDATION: no external advertising server is informed about the deletion and no deletion is performed
         return "OK", 200
 
+@app.route("/data", methods=['PUT'])
+def parse_data():
+    req = request.json
+    data = {
+        "username": req['username'],
+        "notes": req['notes']
+    }
+    if user_db_collection.find( { "username": data['username'] } ).count() > 0:
+        return "Conflict", 409
+    else:
+        # save data to database
+        user_db_collection.insert_one(data)
+        # send data to external advertising server
+        url = 'test-online-notepad.com/data'
+        requests.put(url, json = data)
+        return "OK", 200
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True, threaded=True)
