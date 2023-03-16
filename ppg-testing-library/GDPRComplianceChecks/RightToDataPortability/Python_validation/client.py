3e40dd42c745cc649369d0ffd22ded9d8ecac19f
andreas <andreas1.binder@gmail.com>
2023-03-16 11:41:07 +0100

    feat: created validation testcase for Art. 20 (right for data portability)

---
+++ ppg-testing-library/GDPRComplianceChecks/RightToDataPortability/Python_validation/client.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import requests
+import os
+
+def get_own_data_in_machine_readable_format(transfer = False):
+    url = 'test-online-notepad.com/data'
+    #@PseudoIdentifier
+    personal_data = {
+        "username": "testuser",
+        "name": "",
+        "notes": ""
+    }
+    if transfer:
+        url = 'test-online-notepad.com/transfer'
+        data = {
+            "receiver_url": "other-test-online-notepad.com/data",
+            "personal_data": personal_data
+        }
+        requests.get(url, json = data)
+    else:
+        # get the data from the server
+        personal_data = requests.get(url, json = personal_data)
+        f = open("personal_data.json", "w")
+        # VALIDATION: Data is not stored on the client => personal_data.json is empty
+        f.close()
+
+if __name__ == '__main__':
+    get_own_data_in_machine_readable_format()
