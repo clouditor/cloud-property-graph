77bebb2e57eebff76ce276f39d37f5c2b073876d
andreas <andreas1.binder@gmail.com>
2023-04-09 21:40:08 +0200

    add: missing addition to translationResult

--- cloudpg/src/main/java/io/clouditor/graph/nodes/labels/PseudoIdentifier.kt
+++ cloudpg/src/main/java/io/clouditor/graph/nodes/labels/PseudoIdentifier.kt
@@ -4,6 +4,7 @@ import de.fraunhofer.aisec.cpg.graph.Node
 
 open class PseudoIdentifier(labeledNode: Node) : DataLabel(labeledNode) {
     override fun areMergeable(l: Label): Boolean {
-        return l::class == PseudoIdentifier::class
+        return false
+        //return l::class == PseudoIdentifier::class
     }
 }
--- cloudpg/src/main/java/io/clouditor/graph/passes/FileWritePass.kt
+++ cloudpg/src/main/java/io/clouditor/graph/passes/FileWritePass.kt
@@ -5,6 +5,7 @@ import de.fraunhofer.aisec.cpg.graph.statements.expressions.CallExpression
 import de.fraunhofer.aisec.cpg.passes.Pass
 import io.clouditor.graph.Application
 import io.clouditor.graph.FileWrite
+import io.clouditor.graph.plusAssign
 
 abstract class FileWritePass: Pass() {
 
@@ -17,6 +18,8 @@ abstract class FileWritePass: Pass() {
         val fileWriteNode = FileWrite(call)
         // Add to functionalities if necessary
         app?.functionalities?.plusAssign(fileWriteNode)
+        // Add to translation result
+        t += fileWriteNode
         return fileWriteNode
     }
 }
