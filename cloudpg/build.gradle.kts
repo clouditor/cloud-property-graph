892d13f0e545299aaf77f1accad4f2748b2bf165
Robert Haimerl <robert.haimerl@tum.de>
2023-06-01 11:31:22 +0200

    bump java target form 11 to 17

--- cloudpg/build.gradle.kts
+++ cloudpg/build.gradle.kts
@@ -31,13 +31,13 @@ java {
     sourceSets["main"].java {
         srcDir("${generatedDir}/main/java")
     }
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
 }
 
