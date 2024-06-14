026229ca0ab0d022fb95a4c0a0772c50319d27b1
andreas <andreas1.binder@gmail.com>
2024-06-14 14:53:43 +0200

    fix: expected number of pathes

--- cloudpg/src/test/java/io/clouditor/graph/LinkabilityTest.kt
+++ cloudpg/src/test/java/io/clouditor/graph/LinkabilityTest.kt
@@ -23,7 +23,7 @@ open class LinkabilityTest {
                 listOf(Path(".")),
                 "MATCH p=(:PseudoIdentifier)--()-[:DFG*]->(h:HttpEndpoint) RETURN p"
             )
-        assertEquals(8, result.count())
+        assertEquals(4, result.count())
 
         result.first().apply {
             var path = this.get("p") as Array<*>
@@ -133,7 +133,7 @@ open class LinkabilityTest {
                 listOf(Path(".")),
                 "MATCH p=(:PseudoIdentifier)--()-[:DFG*]->(h1:HttpRequest)-[:DFG*]->(h2:HttpRequest), (a1:Application), (a2:Application) WHERE (h1)--(a1) AND (h2)--(a2) RETURN p"
             )
-        assertEquals(1, result.count())
+        assertEquals(2, result.count())
 
         result.first().apply {
             val path = this.get("p") as Array<*>
